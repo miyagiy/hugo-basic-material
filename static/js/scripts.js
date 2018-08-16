ecc4b7e9e01a879692868361d57cde2608238753
miyagiy <miyagiyuki0430@gmail.com>
2018-08-17 00:19:37 +0900

    site-header transition

---
+++ static/js/scripts.js
@@ -0,0 +1,33 @@
+window.addEventListener("scroll", onScroll)
+
+
+var lastScrollY = 0;
+
+function getElement(element, className) {
+    return element.getElementsByClassName(className)[0]
+}
+
+function onScroll() {
+    const y = window.scrollY
+
+    transitionHeader(y, lastScrollY);
+
+    lastScrollY = window.scrollY;
+}
+
+function transitionHeader(y, lastScrollY) {
+    const shownClassName = "site-header-shown";
+    const hiddenClassName = "site-header-hidden";
+    const siteHeader = getElement(document, "site-header");
+    const diffY = y - lastScrollY;
+
+    if (diffY < 0) { // up
+        if (!siteHeader.classList.contains(shownClassName)) {
+            siteHeader.classList.replace(hiddenClassName, shownClassName);
+        }
+    } else if (diffY > 0) { // down
+        if (!siteHeader.classList.contains(hiddenClassName)) {
+            siteHeader.classList.replace(shownClassName, hiddenClassName);
+        }
+    }
+}
